983fc567cfd78863c5cd82a595abbbf04fd74d1e
lfj-io <73473330+lfj-io@users.noreply.github.com>
2021-01-21 07:34:47 +0700

    Update forum.js

--- js/forum.js
+++ js/forum.js
@@ -1,8 +1,4 @@
- $(document).ready(function () {
 var langVN=document.querySelector('a[href*="misc/language"]').innerText.match(/ng\sVi/ig)?true:false;
 var langEN=document.querySelector('a[href*="misc/language"]').innerText.match(/english/ig)?true:false;
 var langZH=false;
 if(langEN ==false && langVN==false){langZH=true; }
- 
- 
- })
