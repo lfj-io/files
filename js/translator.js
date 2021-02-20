2b21ceb27204309d0c63ded3378a1d91934094b6
lfj-io <73473330+lfj-io@users.noreply.github.com>
2021-02-20 15:43:14 +0700

    Update translator.js

--- js/translator.js
+++ js/translator.js
@@ -1,6 +1,9 @@
 var threads_32_CN = '<h2>如何写好问题报告</h2>所有伟大的项目都是在用户反馈的基础上建立起来的。LFJ利用公众社区来对反馈进行跟踪。本帖详细介绍了如何才能编写出让开发人员易于阅读和做出反应的问题报告，这会让你的故障报告或功能请求更有可能得到解决!<br/><br/><h2>准备工作</h2><h3><div style="margin-left: 20px">检查同类问题</div></h3><div style="margin-left: 20px">在编写任何类型的问题之前，最好先查看是否已经存在匹配的问题。如果有一样的问题，你就不用再耗费精力自行编写了! 因此在你开始编写问题之前，<a href="https://bbs.lfj.io/?search/&type=post" class="link link--internal">在这里查看</a>一下有没有重复或相关的问题吧。</div><div class="bbCodeSpoiler"><button type="button" class="bbCodeSpoiler-button button--longText button" data-xf-click="toggle" data-xf-init="tooltip" title="点击显示隐藏内容"><span class="button-text"><span>隐藏: <span class="bbCodeSpoiler-button-title">报告故障</span></span></span></button><div class="bbCodeSpoiler-content"><div class="bbCodeBlock bbCodeBlock--spoiler"><div class="bbCodeBlock-content"><h3>报告故障</h3>你发现了一个故障? 太棒了! 我们喜欢收到故障报告，因为我们希望这个项目可以尽可能地稳定运行。以下是在解决故障时，你可以遵循的步骤<br/><div style="margin-left: 20px"><ol><li data-xf-list-type="ol">确保你的AdGuard, AdBlock, Ublock Origin和其他广告拦截器被完全禁用[学习:<a href="https://i.imgur.com/wwMEjfz.gif" target="_blank" class="link link--external" rel="noopener">如何禁用一个扩展插件</a>]</li><li data-xf-list-type="ol">确保你使用的<a href="https://violentmonkey.github.io/get-it/" target="_blank" class="link link--external" rel="noopener">Violentmonkey</a>和其他类似的插件，比如Tampermonkey, Greasemonkey等等，<span style="color: rgb(226, 80, 65)">已经被完全禁用或卸载。</span></li><li data-xf-list-type="ol">确保您使用的是lfi.io（不是sleazyfork）的正式版本。</li><li data-xf-list-type="ol">检查同类问题</li><li data-xf-list-type="ol">收集证据</li><li data-xf-list-type="ol">确保故障可再现</li><li data-xf-list-type="ol">起因建议</li><li data-xf-list-type="ol">写下你的问题!</li></ol></div><h4>收集证据</h4>一般来说，关于故障的信息越多越好。以下是你需要提供的一些信息:<br/>如果故障导致了视觉显示问题，那么截屏、gif或视频会非常有帮助。<br/><br/><h4>确保故障可再现</h4>只有当故障可以再现时，我们才可以修复它，因此在提交一个问题之前，你需要确保你有一个可靠的方法让故障再现。<br/>您应该编写一个带有编号的步骤列表，告诉开发人员如何再现错误。例如:<br/><div style="margin-left: 20px"><ol><li data-xf-list-type="ol">打开pornhub.com。</li><li data-xf-list-type="ol">转到Y页。</li><li data-xf-list-type="ol">在X处点击Z。</li><li data-xf-list-type="ol">观察问题，它看起来像W。</li></ol></div><br/><h4>起因建议</h4>如果您认为您知道错误发生的原因，那么请把您的建议也加进去。再强调一遍，尽可能具体。<br/><br/><h4>写下你的问题!</h4>现在是编写故障报告的时候了。<a href="https://bbs.lfj.io/?forums/5/post-thread" class="link link--internal">创建你的文章</a><br/>请确保列出问题模板的所有部分，即使是这里没有详细说明的部分。<br/>感谢对故障报告展现出的兴趣，希望你能享受撰写问题报告的乐趣!</div></div></div></div><div class="bbCodeSpoiler"><button type="button" class="bbCodeSpoiler-button button--longText button" data-xf-click="toggle" data-xf-init="tooltip" title="点击显示隐藏内容"><span class="button-text"><span>隐藏: <span class="bbCodeSpoiler-button-title">功能请求</span></span></span></button><div class="bbCodeSpoiler-content"><div class="bbCodeBlock bbCodeBlock--spoiler"><div class="bbCodeBlock-content"><h3>功能请求</h3>你想做出一些改变让LFJ变得更好吗? 你有一个关于插件，范例，或者代码实验室的构想吗? 也许你已经有了一个自己热衷的想法，并且已经找到了提高的办法。如果是这样的话，那么恭喜你，你来对地方了! 以下是可以帮助您创建可获得响应的优质功能请求的步骤。<br/><div style="margin-left: 20px"><ol><li data-xf-list-type="ol">检查同类问题</li><li data-xf-list-type="ol">检查要求</li><li data-xf-list-type="ol">撰写你的功能请求!</li></ol></div><h4>检查要求</h4>我们愿意让每个想法都能成为这个项目的一部分! 但遗憾的是，我们是普通人，所以我们对可接纳的请求有指导方案。<br/>以下是指导方案:<br/><div style="margin-left: 20px"><ul><li data-xf-list-type="ul"><a href="https://bbs.lfj.io/?threads/25/" class="link link--internal">指导方案 新网站</a></li></ul></div>但这并不是硬性规定，这只是为了让你在投入时间创建你的功能请求之前，明白我们想要的请求是什么样的。<br/>在提交请求之前，你不一定需要使用华丽的装饰和图表，但你应该对自己想要的东西有一个明确的想法。在撰写之前，不妨考虑一下以下这些问题:<br/><div style="margin-left: 20px"><ul><li data-xf-list-type="ul">为什么我想要这个功能?</li><li data-xf-list-type="ul">这个功能解决什么问题了吗?</li><li data-xf-list-type="ul">这个功能的目标用户是谁?</li><li data-xf-list-type="ul">为什么这个功能适合他们?</li><li data-xf-list-type="ul">有哪些替代方案可以达到同样的效果?</li></ul></div>一旦你弄清楚了这些东西，你就会获得优质的功能请求!<br/><h4>撰写你的功能请求!</h4>现在，您已经准备好编写功能请求了:<a href="https://bbs.lfj.io/?forums/5/post-thread" class="link link--internal">创建您的文章</a>。<br/>请确保列出问题模板的所有部分，即使是这里没有详细说明的部分。<br/>感谢对功能请求展现出的兴趣，希望你能享受撰写的乐趣!</div></div></div></div>';
 var threads_25_CN = '<h4><span style="font-size: 18px">您可以在本主题的下一个更新中添加希望LFJ支持这些网站的网站。</span></h4><ul><li data-xf-list-type="ul">根据您的要求，该网站必须具有免费/或试用版，且无需信用卡。 如果这些网站没有免费/试用版选项，则必须包括这些网站的付费帐户（ <a href="https://lfj.io/#contacts" target="_blank" class="link link--external" rel="noopener">您可以在此处发送帐户</a>）</li><li data-xf-list-type="ul">请检查评论或[ <a href="https://bbs.lfj.io/index.php?search/&type=post" class="link link--internal">搜索</a>]以确保您的请求是唯一的。 如果您看到有人已经请求，则只需对该评论进行投票。</li></ul>';
 var threads_32TITLE_CN='如何写好问题报告';
+var guest_CN='你想看的内容只是提供给成员。';
+var guest_EN='The content you want to see is only available to members.';
+var guest_VI='Nội dung bạn muốn xem chỉ dành cho thành viên. ';
 var langVN=document.querySelector('a[href*="misc/language"]').innerText.match(/ng\sVi/ig)?true:false;
 var langEN=document.querySelector('a[href*="misc/language"]').innerText.match(/english/ig)?true:false;
 var langZH=false;
@@ -8,6 +11,13 @@ if(langEN ==false && langVN==false){langZH=true; }
 
 
 	if(langVN==true){
+		
+		if(/(forums\/4|threads)/.test(window.location.search)==false && /(forums\/)/.test(window.location.search) ==true){
+		var h3x2= document.createElement('h3')
+		h3x2.innerHTML=guest_VI;
+		h3x2.align='center';
+		document.querySelector('.block-body .structItem.structItem').replaceChildren(h3x2)
+		}
 		if(document.querySelector('.media__body .uix_welcomeSection__title')){
 		document.querySelector('.media__body .uix_welcomeSection__title').innerText='Chào mừng đến với cộng đồng trợ giúp của LFJ';
 		document.querySelector('.media__body .uix_welcomeSection__text').innerText='Đây là nơi để người dùng LFJ đặt câu hỏi hoặc cung cấp phản hồi về các sản phẩm của LFJ. Người dùng LFJ có thể thảo luận về sản phẩm có liên quan hoặc cung cấp mẹo để sử dụng LFJ cho cộng đồng,cũng như nhận trợ giúp từ những người dùng LFJ khác.';
@@ -21,6 +31,12 @@ if(langEN ==false && langVN==false){langZH=true; }
 		if(document.querySelector('a[href*="threads/25"]')){document.querySelector('a[href*="threads/25/"]').innerText='Bạn muốn đề xuất trang web mới? hãy đọc bài này!';}
 
 	} else if(langZH==true){
+		if(/(forums\/4|threads)/.test(window.location.search)==false && /(forums\/)/.test(window.location.search) ==true){
+		var h3x2= document.createElement('h3')
+		h3x2.innerHTML=guest_CN;
+		h3x2.align='center';
+		document.querySelector('.block-body .structItem.structItem').replaceChildren(h3x2)
+		}
 		if(document.querySelector('.media__body .uix_welcomeSection__title')){
 		document.querySelector('.media__body .uix_welcomeSection__title').innerText='欢迎来到LFJ的帮助社区';
 		document.querySelector('.media__body .uix_welcomeSection__text').innerText='LFJ 用户可以在这里提出问题或提供有关 LFJ 产品和服务的反馈意见，与其他 LFJ 用户和爱好者讨论产品和服务，向社区提供使用 LFJ 产品和服务的技巧，并获得其他用户的帮助。';
@@ -36,6 +52,14 @@ if(langEN ==false && langVN==false){langZH=true; }
 		if(/(\/32\/)/g.test(location.search)){document.querySelector('article .bbWrapper').innerHTML=threads_32_CN;}
 		if(/(\/25\/)/g.test(location.search)){document.querySelector('article .bbWrapper').innerHTML=threads_25_CN;}
 		
+	} else{
+	
+		if(/(forums\/4|threads)/.test(window.location.search)==false && /(forums\/)/.test(window.location.search) ==true){
+		var h3x2= document.createElement('h3')
+		h3x2.innerHTML=guest_EN;
+		h3x2.align='center';
+		document.querySelector('.block-body .structItem.structItem').replaceChildren(h3x2)
+		}
 	}
 //___________________________________
 var Aes={cipher:function(c,e){for(var a=e.length/4-1,d=[[],[],[],[]],b=0;16>b;b++)d[b%4][Math.floor(b/4)]=c[b];d=Aes.addRoundKey(d,e,0,4);for(b=1;b<a;b++)d=Aes.subBytes(d,4),d=Aes.shiftRows(d,4),d=Aes.mixColumns(d,4),d=Aes.addRoundKey(d,e,b,4);d=Aes.subBytes(d,4);d=Aes.shiftRows(d,4);d=Aes.addRoundKey(d,e,a,4);a=Array(16);for(b=0;16>b;b++)a[b]=d[b%4][Math.floor(b/4)];return a},keyExpansion:function(c){for(var e=c.length/4,a=e+6,d=Array(4*(a+1)),b=Array(4),f=0;f<e;f++)d[f]=[c[4*f],c[4*f+1],c[4*f+2],
